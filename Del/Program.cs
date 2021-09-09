eca2ef896dc4e2a099f3f915a894a234d63929b9
HelenaPar <88205255+HelenaPar@users.noreply.github.com>
2021-09-09 21:25:05 +0300

    Add files via upload

---
+++ Del/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Del
+{
+    class Program
+    {
+        delegate int Operation(int a, int b);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                char operation = Convert.ToChar(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                DoOperation(a, b, operation);
+                Console.WriteLine();
+            }
+        }
+        private static void DoOperation(int a, int b, char operation)
+        {
+            switch(operation)
+            {
+                case '+':
+                    Operation add = (a, b) => a + b;
+                    Console.WriteLine("Результат: " + add(a, b));
+                    break;
+                case '-':
+                    Operation sub = (a, b) => a - b;
+                    Console.WriteLine("Результат: " + sub(a, b));
+                    break;
+                case '*':
+                    Operation mul = (a, b) => a * b;
+                    Console.WriteLine("Результат: " + mul(a, b));
+                    break;
+                case '/':
+                    Operation div = (a, b) =>
+                    {
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Делить на ноль нельзя!");
+                            return 0;
+                        }
+                        else return (a / b);
+                    };
+                    Console.WriteLine("Результат: " + div(a, b));
+                    break;
+                default:
+                    Console.WriteLine("Не верный знак!");
+                    break;
+            }
+        }
+    }
+}
