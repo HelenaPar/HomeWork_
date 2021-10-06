3fe66bd929b84e9b10a465fb5ea34c5102e689a3
HelenaPar <88205255+HelenaPar@users.noreply.github.com>
2021-10-06 19:12:05 +0300

    Add files via upload

---
+++ Async/CreditCalculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    internal class CreditCalculator
+    {
+        private readonly Repository repository = new Repository();
+
+        public async Task<CreditInfo> Calculate()
+        {
+            int clientId = await repository.GetClientId();
+            Task<long> creditId = repository.GetCreditId(clientId);
+            Task<string> fullname = GetFullName(clientId);
+            Task<int> paidAmount = GetPaidAmount(creditId.Result);
+            Task<int> leftToPay = GetLeftToPay(creditId.Result);
+            
+            return new CreditInfo
+            {
+                FullName = fullname.Result,
+                PaidAmount = paidAmount.Result, 
+                LeftToPay = leftToPay.Result
+            };
+        }
+
+        private async Task<string> GetFullName(int clientId)
+        {
+            Task<string> getfirstName = repository.GetFirstName(clientId);
+            Task<string> getlastName = repository.GetLastName(clientId);
+            return $"{await  getfirstName} {await getlastName}";
+        }
+
+        private async Task<int> GetLeftToPay(long creditId)
+        {
+            Task<int> creditAmount = GetCreditAmount(creditId);
+            Task<double> interestCharges = GetInterestCharges(creditId);
+            Task<int> paidAmount = GetPaidAmount(creditId);
+            var left = await creditAmount + await interestCharges - await paidAmount;
+            return (int)left;
+        }
+
+        private async Task<double> GetInterestCharges(long creditId)
+        {
+            Task<int> creditAmount = GetCreditAmount(creditId);
+            Task<int> monthlyRate = repository.GetMonthlyRate(creditId);
+            Task<int> creditTerm = repository.GetCreditTerm(creditId);
+            return await creditAmount * await monthlyRate / 100 * await creditTerm;
+        }
+
+        private async Task<int> GetPaidAmount(long creditId)
+        {
+            DateTime dateOfCredit = repository.GetDateOfCredit(creditId);
+            int months = 12 * (DateTime.Now.Year - dateOfCredit.Year) + DateTime.Now.Month - dateOfCredit.Month;
+            Task<int> monthlyPayment = repository.GetMonthlyPayment(creditId);
+            return months * await monthlyPayment;
+        }
+
+        private async Task<int> GetCreditAmount(long creditId)
+        {
+            Task<int> creditAmount = repository.GetCreditAmount(creditId);
+            return await creditAmount;
+        }
+    }
+}
---
+++ Async/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            CreditCalculator calculator = new CreditCalculator();
+            Stopwatch sw = Stopwatch.StartNew();
+            CreditInfo info = await calculator.Calculate();
+
+            Console.WriteLine($"Время загрузки: {sw.Elapsed}");
+            Console.WriteLine(info);
+            Console.ReadKey();
+        }
+    }
+}
