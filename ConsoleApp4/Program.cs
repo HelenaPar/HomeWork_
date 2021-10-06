ffb70022b5dc074471c06c0eb1c6e9990129e3e0
HelenaPar <88205255+HelenaPar@users.noreply.github.com>
2021-10-06 19:41:55 +0300

    Update Program.cs

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -8,21 +8,46 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            //Timer timer = new Timer(() => Console.WriteLine("///"), 1);
-            //timer.Start();
-            //Thread.Sleep(5000);
-            //timer.Restart();
-
-            int[] array = Enumerable.Range(1, 1000_000).ToArray();
-            int count = 0;
-            foreach (int item in array)
-            {
-                count += item;
+            int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    case 1:
+                        Timer timer = new Timer(() => Console.WriteLine("///"), 1);
+                        timer.Start();
+                        Thread.Sleep(50);
+                        //  timer.Stop();
+                        timer.Restart();
+                        break;
+                    case 2:
+                        int[] array = Enumerable.Range(1, 1000_000).ToArray();
+                        int count = 0;
+                        foreach (int item in array)
+                        {
+                            count += item;
+                        }
+                        Console.WriteLine("Sum without threads = " + count);
+                        Counter counter = new Counter(array);
+                        counter.FinishEvent += Work;
+                        Console.WriteLine();
+                        Console.WriteLine("Sum in all threads = " + counter.Calculate());
+                        break;
+                    default:
+                        Console.WriteLine("Enter 1 or 2");
+                        break;
+                
             }
-            Console.WriteLine("Sum without task = " + count);
-            Counter counter = new Counter(array);
-            Console.WriteLine();
-            Console.WriteLine("Sum in all tasks = " + counter.Calculate());
+        }
+
+        private static void Work(object obj, EventArgs eventArgs)
+        {
+            var mesEventArgs = eventArgs as MesEventArgs;
+            if (mesEventArgs == null)
+            {
+                return;
+            }
+            Console.WriteLine("Work: " + mesEventArgs.Work);
+            Console.WriteLine("Not work: " + mesEventArgs.NotWork);
         }
     }
 }
+
