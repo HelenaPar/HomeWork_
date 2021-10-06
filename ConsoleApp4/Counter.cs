4f22277f2e33c47ec8314b50be86d00f666050bf
HelenaPar <88205255+HelenaPar@users.noreply.github.com>
2021-10-06 19:39:59 +0300

    Update Counter.cs

--- ConsoleApp4/Counter.cs
+++ ConsoleApp4/Counter.cs
@@ -9,18 +9,30 @@ namespace ConsoleApp4
 {
     class Counter
     {
-        private int[] array = new int[1000_000];
+         private int[] array = new int[1000_000];
         private int totalCount;
-        private static int work;
-        private static int nwork;
-        static Work workk;
+        private static int work = 10;
+        private static int notWork = 0;
+        private EventHandler finishEvent;
+
+        public event EventHandler FinishEvent
+        {
+            add
+            {
+                finishEvent += value;
+            }
+            remove
+            {
+                finishEvent -= value;
+            }
+        }
+
         public Counter(int[] array)
         {
             this.array = array;
         }
         public long Calculate()
         {
-            workk = new Work();
             Task[] tasks = new Task[10];
             for (int i = 0; i < 10; i++)
             {
@@ -31,14 +43,12 @@ namespace ConsoleApp4
                     foreach (int item in partOfArray)
                     {
                         Interlocked.Add(ref totalCount, item);
-                        work = 10 - i;
-                        nwork = i;
                     }
                 });
                 tasks[i].RunSynchronously();
-                workk.Event += Message;
-                workk.Event += Work;
-                workk.InitializeEvent();
+                Interlocked.Decrement(ref work);
+                Interlocked.Increment(ref notWork);
+                finishEvent?.Invoke(this, new MesEventArgs(work, notWork));
                 Thread.Sleep(1000);
             }
             foreach (var item in tasks)
@@ -47,21 +57,6 @@ namespace ConsoleApp4
             }
             return totalCount;
         }
-
-        private static void Work(object obj, EventArgs eventArgs)
-        {
-            var MesEventArgs = eventArgs as MesEventArgs;
-            if (MesEventArgs == null)
-            {
-                return;
-            }
-            Console.WriteLine("Work: " + work);
-            Console.WriteLine("Not work: " + nwork);
-        }
-
-        private static void Message(object obj, EventArgs eventArgs)
-        {
-            Console.WriteLine(eventArgs.ToString());
-        }
     }
 }
+
